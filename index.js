c3eb184932a2c3733766fb9d3b7a0b506201ca7b
Miraurie <laurie.haillot@hotmail.fr>
2019-04-26 11:10:30 +0200

    code

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -10,5 +9,12 @@ client.on('message', msg => {
     msg.reply('Pong!');
   }
 });
+client.on('message', (message) => {
+  if (member.roles.find('test'))
+      {      
+      	var str = message.content
+        message.reply("said: " + str);
+      } 
+  });
 
 client.login('NTcxMjM3Mzk0MDgzMzQ4NDkz.XMK3Yw.gTdKztlDmMZSe6G8dSudOgdNjso');
